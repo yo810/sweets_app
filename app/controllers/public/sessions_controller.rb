46627230c8926e28ec5777bf4a8eb4973b1310b5
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2022-11-08 14:42:29 +0000

    [課題3] deviseコントローラ準備

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+  protected
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted == true
+      flash[:notice] = "退会済みの為、再登録が必要です。"
+      redirect_to customers_sign_up_path #ここのURL確認する
+    end
+  end
+end
