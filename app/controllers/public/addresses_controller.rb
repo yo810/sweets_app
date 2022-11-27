d23bad518de6b923aa32022d2578d817e4baa182
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2022-11-27 19:17:24 +0000

    add mypage修正,addresses一覧作成途中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,13 +1,20 @@
 class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
-    @addresses = current_customer.addresses
   end
 
   def edit
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to public_addresses
+    else
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def update
