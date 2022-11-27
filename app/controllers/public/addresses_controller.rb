b61296fb706ce10998df76926d4a847c4934b26a
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2022-11-27 17:31:59 +0000

    address 一覧画面作成途中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,7 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
   end
 
   def edit
@@ -12,5 +14,11 @@ class Public::AddressesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 end
