6f6e91d24d2d1c08db4183c325e7fb825ff26d7b
EC2 Default User <ec2-user@ip-172-31-45-228.ap-northeast-1.compute.internal>
2022-12-02 11:36:50 +0000

    finish

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.page(params[:page]).per(8)
+    @item_all = Item.all
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -15,12 +15,12 @@ class Item < ApplicationRecord
     (price * 1.1).floor
   end
 
-  # def image(width, height)
-  #   unless image.attached?
-  #     file_path = Rails.root.join('app/assets/images/no.image.jpg')
-  #     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  #   end
-  #   image.variant(resize_to_limit: [width, height]).processed
-  # end
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 
 end
